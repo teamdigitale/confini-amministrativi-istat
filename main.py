cf4faf48d6457c407c0d6600f710b3437b942838
Alessio Cimarelli <alessio@dataninja.it>
2019-12-03 19:15:20 +0100

    Add csv enrichment.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json, topojson, geobuf
 from pathlib import Path
 from simpledbf import Dbf5
 import geopandas as gpd
+import pandas as pd
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
@@ -9,7 +10,11 @@ from zipfile import ZipFile
 with open("sources.json") as f:
     sources = json.load(f)
 
-for source in sources[0:1]:
+for source in sources[0:2]:
+
+    print("Processing %s..." % source["name"])
+
+    source["divisions"] = { division["name"]: division for division in source["divisions"] }
 
     # SHP - Shapefile
     output_shp = Path(source["name"], "shp")
@@ -20,6 +25,8 @@ for source in sources[0:1]:
                 zip_root_dir = next(dir for dir in zfile.namelist() if dir.count('/') == 1)
                 for zip_info in zfile.infolist():
                     zip_info.filename = zip_info.filename.replace(zip_root_dir, "")
+                    for division in source["divisions"].values():
+                        zip_info.filename = zip_info.filename.replace(division["dirname"]+"/", division["name"]+"/").replace(division["filename"]+".", division["name"]+".")
                     if zip_info.filename:
                         zfile.extract(zip_info, output_shp)
 
@@ -32,6 +39,12 @@ for source in sources[0:1]:
             csv_filename.parent.mkdir(parents=True, exist_ok=True)
             dbf = Dbf5(dbf_filename)
             dbf.to_csv(csv_filename)
+        for csv_filename in output_csv.glob("**/*.csv"):
+            df = pd.read_csv(csv_filename)
+            for parent in (source["divisions"][division_id] for division_id in source["divisions"][csv_filename.stem].get("parents",[])):
+                jdf = pd.read_csv(Path(output_csv, parent["name"]+"/", parent["name"]+".csv"))
+                df = pd.merge(df, jdf[[parent["key"]] + parent["fields"]], on=parent["key"])
+            df.to_csv(csv_filename, index = False, columns = [col for col in df.columns if "shape_" not in col.lower()])
 
     # Geojson + Topojson + Geobuf
     output_geojson = Path(source["name"], "geojson")
@@ -53,12 +66,12 @@ for source in sources[0:1]:
             gdf.to_file(geojson_filename, driver="GeoJSON")
 
         # Topojson - https://github.com/topojson/topojson
-        topojson_filename = Path(output_topojson, *shp_filename.parts[2:]).with_suffix('.json')
-        if not topojson_filename.exists():
-            topojson_filename.parent.mkdir(parents=True, exist_ok=True)
-            tj = topojson.Topology(gdf, prequantize=False, topology=True)
-            with open(topojson_filename, 'w') as f:
-                f.write(tj.to_json())
+        # topojson_filename = Path(output_topojson, *shp_filename.parts[2:]).with_suffix('.json')
+        # if not topojson_filename.exists():
+        #     topojson_filename.parent.mkdir(parents=True, exist_ok=True)
+        #     tj = topojson.Topology(gdf, prequantize=False, topology=True)
+        #     with open(topojson_filename, 'w') as f:
+        #         f.write(tj.to_json())
 
         # Geobuf - https://github.com/pygeobuf/pygeobuf
         #geobuf_filename = Path(output_geobuf, *shp_filename.parts[2:]).with_suffix('.pbf')
