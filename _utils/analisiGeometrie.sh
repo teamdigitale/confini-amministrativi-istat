a14ccbc84d8f4f019d35bae0a966a091833a2c24
Alessio Cimarelli <jenkin@dataninja.it>
2019-12-18 18:43:16 +0100

    Add analisiGeometrie script

---
+++ _utils/analisiGeometrie.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cartellaDati="$folder"/../20190101
+
+# crea cartella con l'ouput della pulizia delle geometrie
+mkdir -p "$cartellaDati"/shp_clean
+
+# crea db sqlite e poi inizializzalo come db spaziale; cancellalo se preesiste
+rm -f "$cartellaDati"/analisi.sqlite
+sqlite3 "$cartellaDati"/analisi.sqlite "SELECT load_extension('mod_spatialite');
+SELECT InitSpatialMetadata(1);"
+
+cartella="$cartellaDati"/shp/regioni
+mkdir -p "$cartellaDati"/shp_clean/regioni
+
+# analizza lo shp originale
+sqlite3 "$cartellaDati"/analisi.sqlite "SELECT load_extension('mod_spatialite');
+-- importa shp regioni come tabella virtuale
+CREATE VIRTUAL TABLE regioni USING VirtualShape("$cartella/regioni", UTF-8, 32632);
+-- crea tabella con output check geometrico
+create table regioni_check as select PKUID,GEOS_GetLastWarningMsg() msg,ST_AsText(GEOS_GetCriticalPointFromMsg()) punto from regioni WHERE ST_IsValid(geometry) <> 1;"
+
+# conteggia errori rilevati
+errori=$(sqlite3 "$cartellaDati"/analisi.sqlite "SELECT count(*) FROM regioni_check")
+
+# se ci sono errori crea nuova tabella con geometrie corrette
+if [ "$errori" -gt 0 ]; then
+  sqlite3 "$cartellaDati"/analisi.sqlite "SELECT load_extension('mod_spatialite');
+  CREATE table regioni_clean AS SELECT * FROM regioni;
+  SELECT RecoverGeometryColumn('regioni_clean','geometry',32632,'MULTIPOLYGON','XY');
+  UPDATE regioni_clean SET geometry = ST_MakeValid(geometry) WHERE ST_IsValid(geometry) <> 1;"
+fi
+
+# svuota tabella output
+rm -r "$cartellaDati"/shp_clean/regioni/*
+
+# esporta tabella con log errori file di input
+ogr2ogr -f CSV "$cartellaDati"/shp_clean/regioni/regioni_check.csv "$cartellaDati"/analisi.sqlite regioni_check
+
+# crea shapefile con geometrie corrette
+ogr2ogr "$cartellaDati"/shp_clean/regioni/regioni.shp "$cartellaDati"/analisi.sqlite regioni_clean
+
+# elimina db sqlite
+rm -f "$cartellaDati"/analisi.sqlite
